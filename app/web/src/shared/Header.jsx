125b2b6abc32bdabb2ca3fd2c9553abca7269935
BOLAJI MUILI <bolajimuili@gmail.com>
2021-07-09 12:41:43 +0100

    Completed Week 7 Exercise

--- app/web/src/shared/Header.jsx
+++ app/web/src/shared/Header.jsx
@@ -1,70 +1,64 @@
-import {React, useState} from 'react';
+import { React, useState } from 'react';
 import { useHistory } from 'react-router-dom';
 import { Navbar, Nav, FormLabel, FormControl, Button, Form } from 'react-bootstrap';
-
 const Header = () => {
     const [username, setUsername] = useState('');
     const [blow, setBlow] = useState(false);
     let history = useHistory();
-
     if (document.cookie) {
         function getCookie(name) {
             var nameEQ = name + "=";
             var ca = document.cookie.split(';');
-            for(var i=0;i < ca.length;i++) {
+            for (var i = 0; i < ca.length; i++) {
                 var c = ca[i];
-                while (c.charAt(0)===' ') c = c.substring(1,c.length);
-                if (c.indexOf(nameEQ) === 0) return c.substring(nameEQ.length,c.length);
+                while (c.charAt(0) === ' ') c = c.substring(1, c.length);
+                if (c.indexOf(nameEQ) === 0) return c.substring(nameEQ.length, c.length);
             }
             return null;
         }
-
-        
         const cookieValue = getCookie("uid");
         let cookieExists = cookieValue ? true : false;
         if (cookieExists) {
             fetch(`/api/users/${cookieValue}`)
                 .then(res => res.json())
-                .then(function(response) {
+                .then(function (response) {
                     setUsername(`Hi ${response.firstname}`);
                     setBlow(true);
                 })
         }
     }
-
     // When user clicks the logout link
     function HandleLogout(event) {
         document.cookie = "uid=; expires=Thu, 01 Jan 1970 00:00:00 UTC; path=/;"; // Delete cookie
         history.push('/') // Redirect to home page
     }
-
     return (
+        <header>
             <Navbar bg="primary" expand="lg">
                 <Navbar.Brand href="/">Project Explorer</Navbar.Brand>
                 <Navbar.Collapse>
                     <Form inline name="searchForm">
                         <FormLabel className="sr-only" for="searchForm">Search Projects:</FormLabel>
-                        <FormControl type="text" className="form-control" name="searchForm" placeholder="Search Projects"/>
-                            <Button type="submit" variant="outline-light">Search</Button>
+                        <FormControl type="text" className="form-control" name="searchForm" placeholder="Search Projects" />
+                        <Button type="submit" variant="outline-light">Search</Button>
                     </Form>
                     <Nav className="mr-auto">
                         <Nav.Link href="/projects/submit">Projects</Nav.Link>
                     </Nav>
                     <Nav>
                         {blow ? (
-                        <>
-                            <Nav.Link href="#" id="logout" onClick={HandleLogout}>Logout</Nav.Link>
-                            <Navbar.Text id="username">{username}</Navbar.Text>
-                        </>) :
-                        (<>
-                            <Nav.Link href="/signup" id="login">Sign Up</Nav.Link>
-                            <Nav.Link href="/login" id="login">Login</Nav.Link>
-                        </>)}
+                            <>
+                                <Nav.Link href="#" id="logout" onClick={HandleLogout}>Logout</Nav.Link>
+                                <Navbar.Text id="username">{username}</Navbar.Text>
+                            </>) :
+                            (<>
+                                <Nav.Link href="/signup" id="login">Sign Up</Nav.Link>
+                                <Nav.Link href="/login" id="login">Login</Nav.Link>
+                            </>)}
                     </Nav>
-                
                 </Navbar.Collapse>
             </Navbar>
+        </header>
     )
 }
-
 export default Header;
