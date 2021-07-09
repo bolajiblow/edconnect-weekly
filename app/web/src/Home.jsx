165a4c04a51043407b09c737b59820d7377841fd
BOLAJI MUILI <bolajimuili@gmail.com>
2021-07-09 16:34:37 +0100

    Completed Week 7 Exercise

--- app/web/src/Home.jsx
+++ app/web/src/Home.jsx
@@ -1,21 +1,21 @@
 import React from 'react';
 import { useState, useEffect } from 'react';
-import {Link} from 'react-router-dom';
-import { Button, Jumbotron, Card, Row, Col, Container } from 'react-bootstrap';
+import { Link } from 'react-router-dom';
+import { Button, Jumbotron, Card, Container, CardGroup } from 'react-bootstrap';
 import Layout from './shared/Layout';
 
 const Home = (props) => {
-    const [projects,setProjects] = useState([]);
+    const [projects, setProjects] = useState([]);
     useEffect(() => {
         fetch("api/projects")
-        .then(res => res.json())
-        .then(res => {
-            setProjects(res.slice(0,4))
-        })
-    },[])
+            .then(res => res.json())
+            .then(res => {
+                setProjects(res.slice(0, 4))
+            })
+    }, [])
     return (
         <Layout>
-            <main className='mx-auto'>
+            <Container>
                 <Jumbotron className="mt-4">
                     <h1>Welcome to Project Explorer</h1>
                     <p>Project Explorer ia a repository for final year project across all departments at your institution. You can submit your project and search projects submitted by others to learn from</p>
@@ -23,23 +23,23 @@ const Home = (props) => {
                     <Button variant="secondary" href="./Login" >Login</Button>
                 </Jumbotron>
 
-                <Container>
-                    <Row className="showcase">
-                         {projects.map(project => <Col>
-                            <Card>
-                                <Card.Body >
-                                    <Card.Title><Link to={`/projects/${projects.id}`}>{project.title}</Link></Card.Title>
-                                    <Card.Subtitle>{project.authors}</Card.Subtitle>
-                                    <Card.Text>{project.abstract}</Card.Text>
-                                    <Card.Footer>{project.tags}</Card.Footer>
-                                </Card.Body>
-                            </Card>
-                        </Col>)}
-                    </Row>
-                </Container> <br/><br/>
+                <CardGroup className="showcase">
+
+                    {projects.reverse().slice(0, 4).map(project =>
+                        <Card key={project.id}>
+                            <Card.Body >
+                                <Card.Title><Link to={`/projects/${projects.id}`}>{project.title}</Link></Card.Title>
+                                <Card.Subtitle>{project.authors}</Card.Subtitle>
+                                <Card.Text>{project.abstract.substring(0, 100)}...</Card.Text>
+                                <Card.Footer>{project.tags}</Card.Footer>
+                            </Card.Body>
+                        </Card>
+                    )}
+
+                </CardGroup> <br /><br />
 
 
-            </main>
+            </Container>
         </Layout>
     )
 }
